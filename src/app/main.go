767b0ec6b0ea4b26d34876e2d45ea5b022ff28b2
rema424 <rema.gapp@gmail.com>
2019-01-05 15:10:00 +0900

    #2 EchoでHello World!

--- src/app/main.go
+++ src/app/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
-	m := "Hello, World!"
-	fmt.Println(m)
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Route => handler
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+	})
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
 }
