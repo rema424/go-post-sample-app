38a242efd9da9b9d04eddb2e6222041ed7c22de8
rema424 <rema.gapp@gmail.com>
2019-01-16 01:14:10 +0900

    App Engineローカル開発サーバーで既存アプリを実行する #40

--- src/app/main/app-standalone.go
+++ src/app/main/app-standalone.go
@@ -3,29 +3,18 @@
 package main
 
 import (
-	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"net/http"
 )
 
 func createMux() *echo.Echo {
 	e := echo.New()
-	e.Use(middleware.CORS())
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "This is app-standalone.go createMux.")
-	})
-
 	e.Use(middleware.Recover())
-	e.Use(middleware.Logger())
 	e.Use(middleware.Gzip())
-
-	e.Static("/", "public")
 
 	return e
 }
 
 func main() {
-	fmt.Println("This is app-standalone.go's main.")
 	e.Logger.Fatal(e.Start(":8080"))
 }
